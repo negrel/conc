811cbd20d9430a7e5b3aa4276edeabc21a73356a
Alexandre Negrel <alexandre@negrel.dev>
2025-02-28 18:28:57 +0100

    add golangci-lint to nix flake

--- flake.nix
+++ flake.nix
@@ -3,24 +3,35 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
     let
       outputsWithoutSystem = { };
-      outputsWithSystem = flake-utils.lib.eachDefaultSystem
-        (system:
-          let
-            pkgs = import nixpkgs {
-              inherit system;
+      outputsWithSystem = flake-utils.lib.eachDefaultSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          lib = pkgs.lib;
+        in
+        {
+          devShells = {
+            default = pkgs.mkShell {
+              buildInputs = with pkgs; [
+                go
+                gopls
+                golangci-lint
+              ];
             };
-            lib = pkgs.lib;
-          in
-          {
-            devShells = {
-              default = pkgs.mkShell {
-                buildInputs = with pkgs; [ go gopls ];
-              };
-            };
-          });
+          };
+        }
+      );
     in
     outputsWithSystem // outputsWithoutSystem;
 }
